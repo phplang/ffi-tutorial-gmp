ea80d3cfb35a4cbf654fa5205af87aaaefed71e7
Sara Golemon <pollita@php.net>
2019-05-13 16:28:47 -0400

    Add init/clear and mpz type

--- mygmp.php
+++ mygmp.php
@@ -5,12 +5,42 @@ namespace {
 final class MyGMP {
   private static ?\FFI $instance = null;
 
-  public static function _getInstance(): \FFI {
+  /* @@mpz_t */
+  private object $mpz;
+
+  public function __construct(string $initval = '0') {
+    $instance = self::_getInstance();
+    $this->mpz = $instance->new('mpz_t');
+    $instance->__gmpz_init($this->mpz);
+    $instance->__gmpz_set_str($this->mpz, $initval, 0);
+  }
+
+  public function __destruct() {
+    self::_getInstance()->__gmpz_clear($this->mpz);
+  }
+
+  private static function _getInstance(): \FFI {
     if (self::$instance) {
       return self::$instance;
     }
 
     $header = <<<HEADER
+        typedef unsigned long long int mp_limb_t;
+        typedef long long int mp_limb_signed_t;
+
+        typedef struct {
+          int _mp_alloc;
+          int _mp_size;
+          mp_limb_t *_mp_d;
+        } __mpz_struct;
+        typedef __mpz_struct mpz_t[1];
+        typedef __mpz_struct *mpz_ptr;
+        typedef const __mpz_struct *mpz_srcptr;
+
+        void __gmpz_init(mpz_ptr);
+        void __gmpz_clear(mpz_ptr);
+
+        int __gmpz_set_str(mpz_ptr, const char *, int);
         HEADER;
 
     self::$instance = \FFI::cdef($header, 'libgmp.so');
